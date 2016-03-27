50399e660c5258ae9e74e9a9e855584f4d18b9b3
VitaliyRakytyansky <Rakytyansky@rambler.ru>
2016-03-27 21:35:26 +0300

    fix SmokeSuiteTest

--- src/test/java/allWork/SmokeSuiteTest.java
+++ src/test/java/allWork/SmokeSuiteTest.java
@@ -1,6 +1,7 @@
 package allWork;
 
 import allWork.caterories.Buggy;
+import allWork.featureTests.TodoE2ETest;
 import allWork.featureTests.TodoOperationsAtAllFilterTest;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
@@ -11,7 +12,7 @@ import allWork.caterories.Smoke;
  * Created by Winterfall on 27.03.2016.
  */
 @RunWith(Categories.class)
-@Suite.SuiteClasses(TodoOperationsAtAllFilterTest.class)
+@Suite.SuiteClasses({TodoOperationsAtAllFilterTest.class, TodoE2ETest.class})
 @Categories.IncludeCategory(Smoke.class)
 public class SmokeSuiteTest {
 }
